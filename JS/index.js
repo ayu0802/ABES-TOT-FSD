a8293ccad3402a5fe057316cfd2456037ee72416
ayu0802 <ayushi.agarwal@abes.ac.in>
2025-08-20 10:39:02 +0530

    js data

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+/*-- variable declaration|| JS is dynamic programming Language --*/
+
+// var a=30;
+// console.log(typeof(a));
+
+// let a=12;
+// let a=30;
+
+// console.log(a);
+// console.log(typeof(a));
+
+let a=12;
+if(a>10){
+    let a=30;
+    console.log("a="+a);
+}
+
+console.log(a);
