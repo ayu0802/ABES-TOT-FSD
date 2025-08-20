acbcdf111796242463b6fe95bc4cda893a8393ec
ayu0802 <ayushi.agarwal@abes.ac.in>
2025-08-20 12:09:20 +0530

    updates

---
+++ JS/module/main.js
@@ -0,0 +1,7 @@
+const sum=require('./logic.js');
+const data=sum(12,30);
+console.log("sum="+data);
+
+// const math=require('./logic.js');
+// const data=sum(12,30);
+// console.log("sum="math.add(12,30));
